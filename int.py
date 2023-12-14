0034fe2d48fc202f4d99c0005f3670dfc87c56b3
Aviansh Reddy Challa <115349366+AvinashReddy19@users.noreply.github.com>
2023-12-15 00:16:13 +0530

    Update int.py

--- int.py
+++ int.py
@@ -608,29 +608,29 @@ print("Maximum profit is ",max_val)
 
 #0/1 knapsack
 
-def knapsack(wt, val, W, n): 
-	if n == 0 or W == 0: 
-		return 0
-	if t[n][W] != -1: 
-		return t[n][W] 
 
-	if wt[n-1] <= W: 
-		t[n][W] = max( 
-			val[n-1] + knapsack( 
-				wt, val, W-wt[n-1], n-1), 
-			knapsack(wt, val, W, n-1)) 
-		return t[n][W] 
-	elif wt[n-1] > W: 
-		t[n][W] = knapsack(wt, val, W, n-1) 
-		return t[n][W]  
+def knapSack(W, wt, val, n): 
+    K = [[0 for x in range(W + 1)] for x in range(n + 1)] 
+  
+    # Build table K[][] in bottom up manner 
+    for i in range(n + 1): 
+        for w in range(W + 1): 
+            if i == 0 or w == 0: 
+                K[i][w] = 0
+            elif wt[i-1] <= w: 
+                K[i][w] = max(val[i-1] 
+                              + K[i-1][w-wt[i-1]], 
+                              K[i-1][w]) 
+            else: 
+                K[i][w] = K[i-1][w] 
+  
+    return K[n][W]
 
 profit = [60, 100, 120] 
 weight = [10, 20, 30] 
 W = 50
 n = len(profit) 
-	 
-t = [[-1 for i in range(W + 1)] for j in range(n + 1)] 
-print(knapsack(weight, profit, W, n)) 
+print(knapSack(W, weight, profit, n)) 
 
 
 #job scheduling
